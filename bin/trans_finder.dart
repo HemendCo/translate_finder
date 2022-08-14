a6cc05f5bb76f6407c5e32f8feb05354b7050459
FMotalleb <fmotalleb@gmail.com>
2022-08-14 14:41:06 +0430

    [Fix] fixed watch method now only look after changed files

--- bin/trans_finder.dart
+++ bin/trans_finder.dart
@@ -9,27 +9,35 @@ import 'package:trans_finder/features/save_output/save_output.dart';
 import 'package:trans_finder/features/update_current_locales/update_current_locales.dart';
 
 void main(List<String> arguments) {
-  final config = AppConfig.fromArgs(arguments);
+  coreApp(arguments);
+}
+
+void coreApp(List<String> args) {
+  final config = AppConfig.fromArgs(args);
   deInjector.register(config);
   doTheJob();
   if (config.watch) {
-    for (final i in config.selectedDirectories) {
-      final dir = Directory(join(config.workingDirectory, i));
-      if (dir.existsSync()) {
-        print('watching ${dir.path}');
-        dir.watch(events: FileSystemEvent.modify).listen((event) async {
-          print('change found in ${event.path}');
-          await doTheJob();
-        });
-      } else {
-        print('cannot watch ${dir.path} because it does not exists');
-      }
+    activeWatch(config);
+  }
+}
+
+void activeWatch(AppConfig config) {
+  for (final i in config.selectedDirectories) {
+    final dir = Directory(join(config.workingDirectory, i));
+    if (dir.existsSync()) {
+      print('watching ${dir.path}');
+      dir.watch(events: FileSystemEvent.modify).listen((event) async {
+        print('change found in ${event.path}');
+        await doTheJob([event.path]);
+      });
+    } else {
+      print('cannot watch ${dir.path} because it does not exists');
     }
   }
 }
 
-Future<void> doTheJob() async {
-  final samples = getTransTexts();
+Future<void> doTheJob([List<String>? override]) async {
+  final samples = getTransTexts(override);
   final output = generateMap(samples);
   await saveOutput(output);
   updateCurrentLocalesWith(output);
