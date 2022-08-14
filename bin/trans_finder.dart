cc12895d398b74d565c5d2a4921efd6d832ddef0
FMotalleb <fmotalleb@gmail.com>
2022-08-14 11:16:13 +0430

    [Feat] add watch

--- bin/trans_finder.dart
+++ bin/trans_finder.dart
@@ -1,12 +1,29 @@
+import 'dart:io';
+
 import 'package:trans_finder/core/arg_parser/arg_parser.dart';
 import 'package:trans_finder/core/dependency_injector/basic_dependency_injector.dart';
 import 'package:trans_finder/features/generate_map/generate_map.dart';
 import 'package:trans_finder/features/get_trans_texts/get_trans_texts.dart';
 import 'package:trans_finder/features/save_output/save_output.dart';
+import 'package:trans_finder/features/update_current_locales/update_current_locales.dart';
 
 void main(List<String> arguments) {
   final config = AppConfig.fromArgs(arguments);
   deInjector.register(config);
+
+  if (config.watch) {
+    doTheJob();
+    Directory(config.workingDirectory).watch().listen((event) {
+      doTheJob();
+    });
+  } else {
+    doTheJob();
+  }
+}
+
+void doTheJob() {
   final samples = getTransTexts();
-  saveOutput(generateMap(samples));
+  final output = generateMap(samples);
+  saveOutput(output);
+  updateCurrentLocalesWith(output);
 }
