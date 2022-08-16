b96fa66df3dfa950888684afb17a2d9bf11e9e42
FMotalleb <fmotalleb@gmail.com>
2022-08-16 11:46:12 +0430

    [Feat] clipboard watcher

--- bin/translate_finder.dart
+++ bin/translate_finder.dart
@@ -2,35 +2,55 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:dart_clipboard/dart_clipboard.dart';
 import 'package:path/path.dart';
 import 'package:translate_finder/core/arg_parser/arg_parser.dart';
 import 'package:translate_finder/core/dependency_injector/basic_dependency_injector.dart';
 import 'package:translate_finder/core/statics.dart';
+import 'package:translate_finder/features/clipboard_watcher/clipboard_watcher.dart';
 import 'package:translate_finder/features/generate_map/generate_map.dart';
 import 'package:translate_finder/features/get_translate_texts/get_translate_texts.dart';
 import 'package:translate_finder/features/save_output/save_output.dart';
 import 'package:translate_finder/features/update_current_locales/update_current_locales.dart';
 
-void main(List<String> arguments) {
-  if (arguments.contains('--multi') || arguments.contains('-m')) {
-    final configsFile = File(multiConfigFile);
-    if (configsFile.existsSync()) {
-      final configs = configsFile.readAsStringSync();
-      final configsMap = jsonDecode(configs);
-
-      final configsList = (configsMap as List).map((e) => Map<String, dynamic>.from(e));
-      for (final configMap in configsList) {
-        final config = AppConfig.fromMap(configMap);
-        runApp(config);
-      }
-    } else {
-      print('No configs file found create one named `$multiConfigFile`');
-      exit(64);
-    }
-  } else {
-    final config = AppConfig.fromArgs(arguments);
-    runApp(config);
+class LazyInstance<T> {
+  T? _instance;
+  bool get isInitialized => _instance != null;
+  T get instance {
+    _instance ??= _createInstance();
+    return _instance!;
   }
+
+  final T Function() _createInstance;
+
+  LazyInstance(this._createInstance);
+}
+
+void main(List<String> arguments) {
+  int index = 0;
+  ClipboardWatcher().stream.listen((event) {
+    print(event);
+    print(event.length);
+  });
+  // if (arguments.contains('--multi') || arguments.contains('-m')) {
+  //   final configsFile = File(figsFil);
+  //   if (configsFile.existsSyncl()) {
+  //     final configs = configsFile.readAsStringSync();
+  //     final configsMap = jsonDecode(configs);
+
+  //     final configsList = (configsMap as List).map((e) => Map<String, dynamic>.from(e));
+  //     for (final configMap in configsList) {
+  //       final config = AppConfig.fromMap(configMap);
+  //       runApp(config);
+  //     }
+  //   } else {
+  //     print('No configs file found create one named `$multiConfigFile`');
+  //     exit(64);
+  //   }
+  // } else {
+  //   final config = AppConfig.fromArgs(arguments);
+  //   runApp(config);
+  // }
 }
 
 void runApp(AppConfig config) {
---
+++ lib/features/clipboard_watcher/clipboard_watcher.dart
@@ -0,0 +1,47 @@
+import 'package:bloc/bloc.dart';
+import 'package:dart_clipboard/dart_clipboard.dart';
+
+class ClipboardWatcher extends Cubit<Iterable<String>> {
+  ClipboardWatcher({
+    RegExp? regex,
+    this.delayDuration = const Duration(milliseconds: 35),
+  })  : regex = regex ?? RegExp('.+'),
+        super([]) {
+    _watch();
+  }
+  final RegExp regex;
+  final Duration delayDuration;
+  bool _shouldWatch = true;
+
+  Future<void> _watch() async {
+    while (_shouldWatch) {
+      final contents = Clipboard.getContents();
+      if (regex.hasMatch(contents)) {
+        final items = regex.allMatches(contents);
+        final output = items.map((e) => e.group(0) ?? 'null found in regex');
+        if (isAcceptable(output)) {
+          emit(output);
+        }
+      }
+      await Future.delayed(delayDuration);
+    }
+  }
+
+  bool isAcceptable(Iterable input) {
+    if (state.length == input.length) {
+      for (int i = 0; i < state.length; i++) {
+        if (state.elementAt(i) != input.elementAt(i)) {
+          return true;
+        }
+      }
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  Future<void> close() async {
+    _shouldWatch = false;
+    await super.close();
+  }
+}
